b68abeb8d8b4cc274d2ec4e3ea094279b1f0671b
waseem-banavar <waseem.tabrez.banavar.chandpasha@citibanamex.com>
2020-07-15 20:32:16 -0500

    Initial Commit

---
+++ GnomeTown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GnomeTown
+//
+//  Created by 837676 on 15/07/20.
+//  Copyright © 2020 Syed Developers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
